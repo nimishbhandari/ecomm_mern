952c6de4f6b79dc9c65790522485983b7581eee8
Nimish Bhandari <nimishbhandari727521@gmail.com>
2021-07-21 13:04:31 +0530

    .

---
+++ app/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to ecomm</h1>
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
